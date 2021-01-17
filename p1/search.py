b66641fb16ca2742cb46dcbb7558e6176ae72f1b
Nicholas Phillips <nckphillips@gmail.com>
2021-01-17 11:00:48 -0800

    genSearch

--- p1/search.py
+++ p1/search.py
@@ -72,6 +72,63 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+def genSearch(problem, fringe_data_structure, heuristic=nullHeuristic):
+    """
+    Takes a search problem, a Data Structure to use for the fringe, and an optional heuristic and returns a list
+    of actions.
+    """
+    actions = []
+    visited = []
+    total_cost = 0
+    fringe = fringe_data_structure()
+
+    currentState = problem.getStartState()
+    visited.append(currentState)
+    while True:
+        successors = problem.getSuccessors(currentState)
+        for s in successors:
+            # According to the assignment instructions, only expand nodes we have not visited. Don't put known nodes in
+            # the fringe.
+            if s[0] not in visited:
+                # Fringe cost it real cost to get to the node (total cost to parent + cost of action) and heuristic cost
+                fringe_cost = total_cost + s[2] + heuristic(s[0], problem)
+                s = [actions, s, fringe_cost]
+                # the update method only exists for a priority queue data structure
+                if getattr(fringe, "update", None):
+                    fringe.update(s, fringe_cost)
+                else:
+                    fringe.push(s)
+
+        # Keep popping fringe nodes until we get something we haven't seen before. Even though we don't add nodes
+        # to the fringe that are in visited it is possible that there are multiple paths to an unvisited node and we
+        # have added each path to the fringe.
+        fringe_just_popped = fringe.pop()
+        while fringe_just_popped[1][0] in visited:
+            fringe_just_popped = fringe.pop()
+
+        # Item coming off the fringe looks like [[<list of actions for the parent node>, <successor of parent>, cost]
+        actions = fringe_just_popped[0].copy()
+        currentState = fringe_just_popped[1][0]
+
+        # Total cost to get to this node does not include the heuristic cost used for prioritizing on the fringe
+        total_cost = fringe_just_popped[2] - heuristic(currentState, problem)
+        # Now fringe_just_popped is [<state>, direction, cost]
+        fringe_just_popped = fringe_just_popped[1]
+        actions.append(fringe_just_popped[1])
+        visited.append(currentState)
+        if problem.isGoalState(currentState):
+            break
+
+    return actions
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
@@ -89,145 +146,27 @@ def depthFirstSearch(problem):
     "*** YOUR CODE HERE ***"
     from util import Stack
 
-    actions = []
-    visited = []
-    fringe = Stack()
-
-    currentState = problem.getStartState()
-    visited.append(currentState)
-    while True:
-        successors = problem.getSuccessors(currentState)
-        for s in successors:
-            # According to the assignment instructions, only expand nodes we have not visited. Don't put known nodes in
-            # the fringe.
-            if s[0] not in visited:
-                # Keep the actions taken so far on the fringe with the node to be expanded
-                print("not in visited", s[0])
-                print("visited ", visited)
-                s = [actions, s]
-                fringe.push(s)
-
-        # Keep popping fringe nodes until we get something we haven't seen before. Even though we don't add nodes
-        # to the fringe that are in visited it is possible that there are multiple paths to an unvisited node and we
-        # have added each path to the fringe.
-        fringe_just_popped = fringe.pop()
-        while fringe_just_popped[1][0] in visited:
-            fringe_just_popped = fringe.pop()
-
-        # Item coming off the fringe looks like [[<list of actions for the parent node>, <successor of parent>]
-        actions = fringe_just_popped[0].copy()
-        # Now fringe_just_popped is [<state>, direction, cost]
-        fringe_just_popped = fringe_just_popped[1]
-        currentState = fringe_just_popped[0]
-        actions.append(fringe_just_popped[1])
-        visited.append(currentState)
-        if problem.isGoalState(currentState):
-            break
-
-    return actions
-
-    return actions
+    return genSearch(problem, Stack)
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     from util import Queue
 
-    actions = []
-    visited = []
-    fringe = Queue()
-
-    currentState = problem.getStartState()
-    visited.append(currentState)
-    while True:
-        successors = problem.getSuccessors(currentState)
-        for s in successors:
-            # According to the assignment instructions, only expand nodes we have not visited. Don't put known nodes in
-            # the fringe.
-            if s[0] not in visited:
-                # Keep the actions taken so far on the fringe with the node to be expanded
-                print("not in visited", s[0])
-                print("visited ",visited)
-                s = [actions, s]
-                fringe.push(s)
-
-        # Keep popping fringe nodes until we get something we haven't seen before. Even though we don't add nodes
-        # to the fringe that are in visited it is possible that there are multiple paths to an unvisited node and we
-        # have added each path to the fringe.
-        fringe_just_popped = fringe.pop()
-        while fringe_just_popped[1][0] in visited:
-            fringe_just_popped = fringe.pop()
-
-        # Item coming off the fringe looks like [[<list of actions for the parent node>, <successor of parent>]
-        actions = fringe_just_popped[0].copy()
-        # Now fringe_just_popped is [<state>, direction, cost]
-        fringe_just_popped = fringe_just_popped[1]
-        currentState = fringe_just_popped[0]
-        actions.append(fringe_just_popped[1])
-        visited.append(currentState)
-        if problem.isGoalState(currentState):
-            break
-
-    return actions
+    return genSearch(problem, Queue)
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
     from util import PriorityQueue
 
-    actions = []
-    visited = []
-    total_cost = 0
-    fringe = PriorityQueue()
-
-    currentState = problem.getStartState()
-    while True:
-
-        currentState = problem.getStartState()
-        visited.append(currentState)
-        while True:
-            successors = problem.getSuccessors(currentState)
-            for s in successors:
-                # According to the assignment instructions, only expand nodes we have not visited. Don't put known nodes in
-                # the fringe.
-                if s[0] not in visited:
-                    # Keep the actions taken so far on the fringe with the node to be expanded
-                    fringe_cost = total_cost + s[2]
-                    s = [actions, s, fringe_cost]
-                    fringe.update(s, fringe_cost)
-
-            # Keep popping fringe nodes until we get something we haven't seen before. Even though we don't add nodes
-            # to the fringe that are in visited it is possible that there are multiple paths to an unvisited node and we
-            # have added each path to the fringe.
-            fringe_just_popped = fringe.pop()
-            while fringe_just_popped[1][0] in visited:
-                fringe_just_popped = fringe.pop()
-
-            # Item coming off the fringe looks like [[<list of actions for the parent node>, <successor of parent>, cost]
-            actions = fringe_just_popped[0].copy()
-            total_cost = fringe_just_popped[2]
-            # Now fringe_just_popped is [<state>, direction, cost]
-            fringe_just_popped = fringe_just_popped[1]
-            currentState = fringe_just_popped[0]
-            actions.append(fringe_just_popped[1])
-            visited.append(currentState)
-            if problem.isGoalState(currentState):
-                break
-
-        return actions
-
-    return actions
-
-def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
-    return 0
+    return genSearch(problem, PriorityQueue)
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    from util import PriorityQueue
+
+    return genSearch(problem, PriorityQueue, heuristic)
 
 
 # Abbreviations
